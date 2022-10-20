117bbebbae45224dc2deecdd2d2f05a44b409c60
Rishit Agrawal <torishitagrawal@gmail.com>
2022-10-19 21:00:37 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -115,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Icons.download,
                 size: 24.0,
               ),
-              label: Text('Download'),
+              label: Text('Download1'),
             ),
             OutlinedButton.icon(
               // <-- OutlinedButton
@@ -124,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Icons.download,
                 size: 24.0,
               ),
-              label: Text('Download'),
+              label: Text('Download2'),
             ),
             const Text(
               'You have clicked the button this many times:',
